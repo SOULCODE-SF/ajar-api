9e644cd5ee0177b367ac2b7cfd1f3cd5516a88f3
Lintangeaa <lindprak@gmail.com>
2025-05-23 19:47:54 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import { mysqlConnection } from './libs/config/mysqlConnection.js';
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
